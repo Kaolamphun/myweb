658460a8efdaef65db7edae6677bf3a0284b8bc5
maith <maithaen122@gmail.com>
2023-03-02 13:19:16 +0700

    add page number to models views index

--- app_base/views.py
+++ app_base/views.py
@@ -8,12 +8,30 @@ from datetime import datetime, timedelta
 
 from app_items.models import ItemCategory, ItemProduct
 
+from django.core.paginator import Paginator, EmptyPage , InvalidPage
+
 
 # Create your views here.
 def index(request: HttpRequest):
+    # ຈຳນວນສິນຄ້າທິສະແດງໃນ 1 ໜ້າ
+    items_per_page = int(9)
+
     categories = ItemCategory.objects.order_by("-id")  # categories
-    items = ItemProduct.objects.order_by("-id")
-    context = {'categories': categories, 'items': items}   # item
+    items = ItemProduct.objects.all().order_by("-id")[:items_per_page] # items
+
+    paginator = Paginator(items, items_per_page)
+    try:
+        page = int(request.GET.get('page', '1')) # paginate
+    except:
+        page = 1
+
+    try:
+        itemsPerPage = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        itemsPerPage = paginator.page(Paginator.num_pages)
+
+
+    context = {'categories': categories, 'items': itemsPerPage}   # context
     return render(request, "index.html", context)
 
 
